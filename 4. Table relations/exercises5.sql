a4209b155910d2eab99f0596f0dc43e8c26fb5f9
Veselina S <vesi.as@abv.bg>
2023-09-28 20:24:53 +0300

    update

--- 4. Table relations/exercises5.sql
+++ 4. Table relations/exercises5.sql
@@ -278,6 +278,34 @@ ADD CONSTRAINT fk_countries_rivers_countries
 
 13. SET NULL:
 
+CREATE TABLE customers(
+	id SERIAL PRIMARY KEY,
+	customer_name VARCHAR(30)
+);
+
+CREATE TABLE contacts(
+	id SERIAL PRIMARY KEY,
+	contact_name VARCHAR(30),
+	phone VARCHAR(30),
+	email VARCHAR(30),
+	customer_id INTEGER,
+
+	CONSTRAINT fk_contacts_customers
+		FOREIGN KEY (customer_id)
+			REFERENCES customers(id) ON DELETE SET NULL
+);
+
+INSERT INTO customers(customer_name)
+VALUES ('BlueBird Inc'),
+		('Dolphin LLC');
+
+INSERT INTO contacts(contact_name, phone, email, customer_id)
+VALUES ('John Doe', '(408)-111-1234', 'john.doe@bluebird.dev', 1),
+('Jane Doe', '(408)-111-1235', 'jane.doe@bluebird.dev', 1),
+('David Wright', '(408)-222-1234', 'david.wright@dolphin.dev' ,2);
+
+DELETE FROM customers
+WHERE id = 1;
 
 
 14. Peaks in Rila✶:
