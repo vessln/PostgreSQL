e62687fbbba0f69378c6abfdce89d0405c3e4f3f
Veselina S <vesi.as@abv.bg>
2023-10-02 11:26:25 +0300

    add lab

---
+++ 5. Subqueries and JOINs/lab5.sql
@@ -0,0 +1,75 @@
+1. Towns Addresses:
+
+SELECT
+	t.town_id,
+	t.name,
+	a.address_text
+FROM towns AS t
+JOIN addresses AS a
+	ON a.town_id = t.town_id
+WHERE t.name in ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY t.town_id, a.address_id;
+
+
+2. Managers:
+
+SELECT
+	e.employee_id,
+	CONCAT_WS(' ', e.first_name, e.last_name) AS "full_name",
+	d.department_id,
+	d.name
+FROM employees AS e
+JOIN departments AS d
+	ON e.employee_id = d.manager_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+# or
+
+SELECT
+	e.employee_id,
+	CONCAT_WS(' ', e.first_name, e.last_name) AS "full_name",
+	d.department_id,
+	d.name
+FROM employees AS e
+RIGHT JOIN departments AS d
+	ON e.employee_id = d.manager_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+
+3. Employees Projects:
+
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS "full_name",
+	p.project_id,
+	p.name
+FROM employees AS e
+	JOIN employees_projects AS ep
+		ON e.employee_id = ep.employee_id
+			JOIN projects AS p
+				ON p.project_id = ep.project_id
+WHERE p.project_id = 1;
+
+# or
+
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS "full_name",
+	p.project_id,
+	p.name
+FROM employees AS e
+	JOIN employees_projects AS ep
+		USING(employee_id)
+			JOIN projects AS p
+				USING(project_id)
+WHERE p.project_id = 1;
+
+
+4. Higher Salary:
+
+SELECT
+	COUNT(*)
+FROM employees
+WHERE salary > (SELECT AVG(salary) FROM employees);
