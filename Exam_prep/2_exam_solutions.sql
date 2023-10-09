47bce9bc3d068eb01a952116d5be26dd121ac9b3
Veselina S <vesi.as@abv.bg>
2023-10-09 19:11:29 +0300

    update

--- Exam_prep/2_exam_solutions.sql
+++ Exam_prep/2_exam_solutions.sql
@@ -110,7 +110,7 @@ AND LENGTH(full_name) > 3;
 # or
 
 DELETE FROM clients
-WHERE id NOT IN (SELECT client_id FROM clients)
+WHERE id NOT IN (SELECT client_id FROM courses)
 AND LENGTH(full_name) > 3;
 
 
